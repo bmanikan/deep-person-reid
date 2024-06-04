d4cc071a32cfba80304b890bbfa706f2755c3084
bala manikandan <balamanikandanj2@gmail.com>
2024-06-04 22:21:25 +0530

    cslrwr

--- torchreid/optim/lr_scheduler.py
+++ torchreid/optim/lr_scheduler.py
@@ -1,41 +1,41 @@
 from __future__ import print_function, absolute_import
 import torch
 
-AVAI_SCH = ['single_step', 'multi_step', 'cosine']
+AVAI_SCH = ['single_step', 'multi_step', 'cosine', 'cosine_warm']
 
 
 def build_lr_scheduler(
-    optimizer, lr_scheduler='single_step', stepsize=1, gamma=0.1, max_epoch=1
+    optimizer, name='single_step', stepsize=1, gamma=0.1, max_epoch=1, **kwargs
 ):
     """A function wrapper for building a learning rate scheduler.
 
     Args:
         optimizer (Optimizer): an Optimizer.
-        lr_scheduler (str, optional): learning rate scheduler method. Default is single_step.
-        stepsize (int or list, optional): step size to decay learning rate. When ``lr_scheduler``
-            is "single_step", ``stepsize`` should be an integer. When ``lr_scheduler`` is
+        name (str, optional): learning rate scheduler method. Default is single_step.
+        stepsize (int or list, optional): step size to decay learning rate. When ``name``
+            is "single_step", ``stepsize`` should be an integer. When ``name`` is
             "multi_step", ``stepsize`` is a list. Default is 1.
         gamma (float, optional): decay rate. Default is 0.1.
         max_epoch (int, optional): maximum epoch (for cosine annealing). Default is 1.
 
     Examples::
         >>> # Decay learning rate by every 20 epochs.
-        >>> scheduler = torchreid.optim.build_lr_scheduler(
-        >>>     optimizer, lr_scheduler='single_step', stepsize=20
+        >>> scheduler = torchreid.optim.name(
+        >>>     optimizer, name='single_step', stepsize=20
         >>> )
         >>> # Decay learning rate at 30, 50 and 55 epochs.
-        >>> scheduler = torchreid.optim.build_lr_scheduler(
-        >>>     optimizer, lr_scheduler='multi_step', stepsize=[30, 50, 55]
+        >>> scheduler = torchreid.optim.name(
+        >>>     optimizer, name='multi_step', stepsize=[30, 50, 55]
         >>> )
     """
-    if lr_scheduler not in AVAI_SCH:
+    if name not in AVAI_SCH:
         raise ValueError(
             'Unsupported scheduler: {}. Must be one of {}'.format(
-                lr_scheduler, AVAI_SCH
+                name, AVAI_SCH
             )
         )
 
-    if lr_scheduler == 'single_step':
+    if name == 'single_step':
         if isinstance(stepsize, list):
             stepsize = stepsize[-1]
 
@@ -49,7 +49,7 @@ def build_lr_scheduler(
             optimizer, step_size=stepsize, gamma=gamma
         )
 
-    elif lr_scheduler == 'multi_step':
+    elif name == 'multi_step':
         if not isinstance(stepsize, list):
             raise TypeError(
                 'For multi_step lr_scheduler, stepsize must '
@@ -60,9 +60,13 @@ def build_lr_scheduler(
             optimizer, milestones=stepsize, gamma=gamma
         )
 
-    elif lr_scheduler == 'cosine':
+    elif name == 'cosine':
         scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
             optimizer, float(max_epoch)
         )
+
+    elif name == 'cosine_warm':
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer,T_0= kwargs.get('T_0', 5),
+                                                                         eta_min=kwargs.get('eta_min', 1e-6))
 
     return scheduler
